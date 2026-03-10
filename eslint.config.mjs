252f3a3b454a93ee1453710ff7057bfbc6daf866
abdullahmia <abdullah149161@bsdi-bd.org>
2026-03-10 19:44:56 +0600

    feat: add Storybook and Button component stories

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,14 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import nextConfig from "eslint-config-next";
 import nextTypescriptConfig from "eslint-config-next/typescript";
 
-const eslintConfig = [
-  ...nextConfig,
-  ...nextTypescriptConfig,
-  {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-empty-object-type": "off",
-    },
+const eslintConfig = [...nextConfig, ...nextTypescriptConfig, {
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-object-type": "off",
   },
-];
+}, ...storybook.configs["flat/recommended"]];
 
 export default eslintConfig;
