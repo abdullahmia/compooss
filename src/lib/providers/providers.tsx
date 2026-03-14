230ade6869ae60fc38c4984d3dd698b1776ab378
abdullahmia <abdullah149161@bsdi-bd.org>
2026-03-14 23:01:36 +0600

    chore: update dependencies and clean up unused connection code

--- src/lib/providers/providers.tsx
+++ src/lib/providers/providers.tsx
@@ -1,13 +1,15 @@
-'use client'
+"use client";
 
 import { queryClient } from "@/lib/config/query.config";
 import { QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
+import { Toaster } from "sonner";
 
 export const Providers: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      <Toaster position="top-right" />
     </QueryClientProvider>
   );
 };
