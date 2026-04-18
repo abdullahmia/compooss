c36dff1371c147181acbbace595a7bc00426daa9
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2026-04-18 09:25:47 +0600

    fix: added better key in JsonDocumnet to stop unecessary rerenderds

--- apps/compooss/src/lib/components/collections/documents-tab.component.tsx
+++ apps/compooss/src/lib/components/collections/documents-tab.component.tsx
@@ -226,7 +226,7 @@ export const DocumentsTab: React.FC<Props> = ({ readOnly = false }) => {
         ) : viewMode === "list" ? (
           documents.map((doc, i) => (
             <JsonDocument
-              key={i}
+              key={p}
               document={doc}
               index={startIndex - 1 + i}
               onEdit={readOnly ? undefined : handleOpenEdit}
